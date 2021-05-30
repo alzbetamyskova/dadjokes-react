7efa47a5ffc6d06de61307079bc7d1399f7d4451
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-05-30 03:26:32 +0200

    je 3:26 ráno, jdu spát, zítra to dodělám, dobrou noc

--- src/index.jsx
+++ src/index.jsx
@@ -4,23 +4,27 @@ import './style.css';
 
 const App = () => {
 
-  let upLikes = 0;
-  let downLikes = 0;
+  <>
+    <h1>Dad Jokes</h1>
+  </>
 
-  const btnUp = document.querySelector('#btn-up');
-  const btnDown = document.querySelector('#btn-down');
+  // let upLikes = 0;
+  // let downLikes = 0;
 
-  btnUp.addEventListener('click', () => {
-    upLikes += 1;
-    const likesUpElm = document.querySelector('#likes-up');
-    likesUpElm.textContent = upLikes;
-  });
+  // const btnUp = document.querySelector('#btn-up');
+  // const btnDown = document.querySelector('#btn-down');
 
-  btnDown.addEventListener('click', () => {
-   downLikes += 1;
-   const likesDownElm = document.querySelector('#likes-down');
-    likesDownElm.textContent = downLikes;
-  });
+  // btnUp.addEventListener('click', () => {
+  //   upLikes += 1;
+  //   const likesUpElm = document.querySelector('#likes-up');
+  //   likesUpElm.textContent = upLikes;
+  // });
+
+  // btnDown.addEventListener('click', () => {
+  //  downLikes += 1;
+  //  const likesDownElm = document.querySelector('#likes-down');
+  //   likesDownElm.textContent = downLikes;
+  // });
 
 };
 
