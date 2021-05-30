48c5295f8cac2820d5e2bf4647ff0153483963da
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-05-30 03:24:13 +0200

    vykradeno html a čistý js

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const App = () => (
-  <>
-  <h1>Dad Jokes</h1>
-  </>
-);
+const App = () => {
+
+  let upLikes = 0;
+  let downLikes = 0;
+
+  const btnUp = document.querySelector('#btn-up');
+  const btnDown = document.querySelector('#btn-down');
+
+  btnUp.addEventListener('click', () => {
+    upLikes += 1;
+    const likesUpElm = document.querySelector('#likes-up');
+    likesUpElm.textContent = upLikes;
+  });
+
+  btnDown.addEventListener('click', () => {
+   downLikes += 1;
+   const likesDownElm = document.querySelector('#likes-down');
+    likesDownElm.textContent = downLikes;
+  });
+
+};
 
 render(<App />, document.querySelector('#app'));
